a396d75371c63d6fb3a39e9ea2027d0779151075
davidten <david10tco+1@gmail.com>
2018-03-16 01:47:34 +0800

    Change the way browser is initialized

--- src/main/java/seedu/address/logic/commands/LinkedInLoginCommand.java
+++ src/main/java/seedu/address/logic/commands/LinkedInLoginCommand.java
@@ -2,15 +2,17 @@ package seedu.address.logic.commands;
 
 import java.io.IOException;
 
+import seedu.address.commons.core.EventsCenter;
 import seedu.address.commons.core.Oauth2Client;
+import seedu.address.commons.events.ui.ShowBrowserRequestEvent;
+import seedu.address.commons.events.ui.ShowHelpRequestEvent;
 import seedu.address.logic.commands.exceptions.CommandException;
 
 /**
  * Allows a user to login to their LinkedIn account
  */
-public class LinkedInLoginCommand extends UndoableCommand {
+public class LinkedInLoginCommand extends Command {
     public static final String COMMAND_WORD = "linkedin_login";
-    public static final String MESSAGE_FAILED_LOGIN = "Unable to login, please try again";
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Logs in to your LinkedIn account ";
 
     public static final String MESSAGE_SUCCESS = "Browser Opened for LinkedIn Authentication";
@@ -18,21 +20,14 @@ public class LinkedInLoginCommand extends UndoableCommand {
     /**
      * Creates a LinkedInLogin log a Salesperson in to LinkedIn
      */
-    public LinkedInLoginCommand() throws IOException {
-        try {
-            Oauth2Client.authenticateWithLinkedIn();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public LinkedInLoginCommand(){
+
     }
 
     @Override
-    public CommandResult executeUndoableCommand() throws CommandException {
-        try {
-            //TODO: login to linkedIn via API
-            return new CommandResult(String.format(MESSAGE_SUCCESS));
-        } catch (Exception e) {
-            throw new CommandException(MESSAGE_FAILED_LOGIN);
-        }
+    public CommandResult execute() {
+        EventsCenter.getInstance().post(new ShowBrowserRequestEvent());
+        return new CommandResult(MESSAGE_SUCCESS);
     }
+
 }
