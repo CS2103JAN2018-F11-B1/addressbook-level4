0004ef4c773aef52f47c3ce420c56b41c0a58fbf
DESKTOP-9K2NI32\Liu Yiming <e0031565@u.nus.edu>
2018-03-22 06:42:38 +0800

    Corrects indentation for PersonCard

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -14,7 +14,7 @@ public class PersonCard extends UiPart<Region> {
 
     private static final String FXML = "PersonListCard.fxml";
     private static final String[] TAG_COLORS =
-            { "blue", "cyan", "green", "magenta", "orange", "pink", "red", "yellow", "teal", "brown" };
+        { "blue", "cyan", "green", "magenta", "orange", "pink", "red", "yellow", "teal", "brown" };
 
     /**
      * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
