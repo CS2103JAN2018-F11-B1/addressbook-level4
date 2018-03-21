07815a8c2740ba096766ea46e1ebbe8c42f04011
DESKTOP-9K2NI32\Liu Yiming <e0031565@u.nus.edu>
2018-03-22 05:15:25 +0800

    Corrects typo for TAG_COLORS

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -13,7 +13,8 @@ import seedu.address.model.person.Person;
 public class PersonCard extends UiPart<Region> {
 
     private static final String FXML = "PersonListCard.fxml";
-    private static final String[] TAG_COLORS = {"blue, cyan, green, magenta, orange, pink, red, yellow, teal, brown"};
+    private static final String[] TAG_COLORS =
+            {"blue", "cyan", "green", "magenta", "orange", "pink", "red", "yellow", "teal", "brown"};
 
     /**
      * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
